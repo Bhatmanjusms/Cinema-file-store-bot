e5dbd8748a09049dffd0ddb20f6e02aba74e9550
btmanjusms4 <147907513+btmanjusms4@users.noreply.github.com>
2023-12-16 13:34:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,7 +74,7 @@ async def start(bot: Client, cmd: Message):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Movies Channel", url="https://t.me/filmyfunda_movies")
+                        InlineKeyboardButton("Movies Channel", url="https://t.me/Cinema_files_links")
                     ],
                     [
                         InlineKeyboardButton("About Bot", callback_data="aboutbot"),
@@ -82,8 +82,8 @@ async def start(bot: Client, cmd: Message):
                         InlineKeyboardButton("Close 🚪", callback_data="closeMessage")
                     ],
                     [
-                        InlineKeyboardButton("Movies Channel", url="https://t.me/filmyfunda_movies"),
-                        InlineKeyboardButton("Owner", url="https://t.me/ded_eye")
+                        InlineKeyboardButton("Movies Channel", url="https://t.me/Cinema_files_links"),
+                        InlineKeyboardButton("Owner", url="https://t.me/OneLuffyD")
                     ]
                 ]
             )
@@ -124,7 +124,7 @@ async def main(bot: Client, message: Message):
                 return
 
         if message.from_user.id in Config.BANNED_USERS:
-            await message.reply_text("Sorry, You are banned!\n\nContact [Support Group](https://t.me/ded_eye)",
+            await message.reply_text("Sorry, You are banned!\n\nContact [Support Group](https://t.me/OneLuffyD)",
                                      disable_web_page_preview=True)
             return
 
@@ -319,7 +319,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                 [
                     [
                         InlineKeyboardButton("Source Codes of Bot",
-                                             url="https://github.com/PredatorHackerzZ/TG-FileStore")
+                                             url="https://github.com/bhatmanjusms")
                     ],
                     [
                         InlineKeyboardButton("Go Home", callback_data="gotohome"),
@@ -337,7 +337,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                 [
                     [
                         InlineKeyboardButton("Source Codes of Bot",
-                                             url="https://github.com/PredatorHackerzZ/TG-FileStore")
+                                             url="https://github.com/bhatmanjusms")
                     ],
                     [
                         InlineKeyboardButton("About Bot", callback_data="aboutbot"),
@@ -354,7 +354,7 @@ async def button(bot: Client, cmd: CallbackQuery):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Movvies channel", url="https://t.me/filmyfunda_movies")
+                        InlineKeyboardButton("Movvies channel", url="https://t.me/Cinema_files_links")
                     ],
                     [
                         InlineKeyboardButton("About Bot", callback_data="aboutbot"),
@@ -362,8 +362,8 @@ async def button(bot: Client, cmd: CallbackQuery):
                         InlineKeyboardButton("Close 🚪", callback_data="closeMessage")
                     ],
                     [
-                        InlineKeyboardButton("Owner", url="https://t.me/ded_eye"),
-                        InlineKeyboardButton("Movies Channel", url="https://t.me/filmyfunda_movies")
+                        InlineKeyboardButton("Owner", url="https://t.me/OneLuffyD"),
+                        InlineKeyboardButton("Movies Channel", url="https://t.me/Cinema_files_links")
                     ]
                 ]
             )
@@ -379,7 +379,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                 user = await bot.get_chat_member(channel_chat_id, cmd.message.chat.id)
                 if user.status == "kicked":
                     await cmd.message.edit(
-                        text="Sorry Sir, You are Banned to use me. Contact my [Owner](https://t.me/ded_eye).",
+                        text="Sorry Sir, You are Banned to use me. Contact my [Owner](https://t.me/OneLuffyD).",
                         disable_web_page_preview=True
                     )
                     return
@@ -401,7 +401,7 @@ async def button(bot: Client, cmd: CallbackQuery):
                 return
             except Exception:
                 await cmd.message.edit(
-                    text="Something went Wrong. Contact my [Owner](https://t.me/ded_eye).",
+                    text="Something went Wrong. Contact my [Owner](https://t.me/OneLuffyD).",
                     disable_web_page_preview=True
                 )
                 return
@@ -411,8 +411,8 @@ async def button(bot: Client, cmd: CallbackQuery):
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("Owner", url="https://t.me/ded_eye"),
-                        InlineKeyboardButton("Movies Channel", url="https://t.me/filmyfunda_movies")
+                        InlineKeyboardButton("Owner", url="https://t.me/OneLuffyD"),
+                        InlineKeyboardButton("Movies Channel", url="https://t.me/Cinema_files_links")
                     ],
                     [
                         InlineKeyboardButton("About Bot", callback_data="aboutbot"),
