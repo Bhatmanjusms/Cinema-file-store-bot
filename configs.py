dded31c4c9d494a29a230eb96903e8e02e4eb0b3
btmanjusms4 <147907513+btmanjusms4@users.noreply.github.com>
2023-12-16 13:30:12 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,21 +3,21 @@
 import os
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID", "977080"))
-	API_HASH = os.environ.get("API_HASH", "0c20c4265501492a1513f91755acd42b")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN", "5991682088:AAFKgMtrZPYy_PeXN_F4Zs4v1n2lJd8nCgY")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME", "thunder_store3_bot")
-	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001896983424"))
-	SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "moneykamalo.com")
-	SHORTLINK_API = os.environ.get('SHORTLINK_API', "c176f08934f241bd3980fcd7b940611ad2cbb3ce")
-	BOT_OWNER = int(os.environ.get("BOT_OWNER", "399726799"))
-	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://abcd:abcd@cluster0.zglig1z.mongodb.net/?retryWrites=true&w=majority")
-	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001577615410")
-	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001896983424")
+	API_ID = int(os.environ.get("API_ID", "28640367"))
+	API_HASH = os.environ.get("API_HASH", "1155ea430bfb927287488f6ffb1ad892")
+	BOT_TOKEN = os.environ.get("BOT_TOKEN", "6611968744:AAHAkFHafwzUkG3zYhxzKEl0-fyczU5aP5g")
+	BOT_USERNAME = os.environ.get("BOT_USERNAME", "Filestoreses_bot")
+	DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1001688280429"))
+	SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "indianshortner.com")
+	SHORTLINK_API = os.environ.get('SHORTLINK_API', "9aab55003ef433625f6ba71ae73a4eb516824852")
+	BOT_OWNER = int(os.environ.get("BOT_OWNER", "1846472978"))
+	DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://abcd:abcd@cluster0.vagppfe.mongodb.net/?retryWrites=true&w=majority")
+	UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
+	LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001688280429")
 	BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "1234567890").split())
 	FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
 	BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
-	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
+	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
 This is a Permanent FileStore Bot of Filmyfunda Movies 
@@ -34,7 +34,7 @@ This is a Permanent FileStore Bot of Filmyfunda Movies
 │
 ├🔸 **Developer:** [Ded eye](https://t.me/ded_eye) 
 │
-├🔸 **movies Updates:** [Movies Channel](https://t.me/filmyfunda_movies)
+├🔸 **movies Updates:** [Movies Channel](https://t.me/Cinema_files_links)
 │
 ╰──────[ 😎 ]───────────⍟
 """
